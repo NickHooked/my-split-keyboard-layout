872dd549c5b7be68272aa3293d9a17fb68aa5882
nickhaakman <n.haakman@comrads.nl>
2023-04-19 13:02:25 +0200

    Lower the miliamps to make it work with an ipad

--- config.h
+++ config.h
@@ -54,3 +54,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
 #endif
+
+#define USB_MAX_POWER_CONSUMPTION 100
